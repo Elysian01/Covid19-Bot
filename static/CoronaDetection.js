241ef8273507e4f4a6de48df5a757165d199fe1f
Abhishek Gupta <abhig0209@gmail.com>
2020-10-03 00:47:42 +0530

    frontend ready

--- static/CoronaDetection.js
+++ static/CoronaDetection.js
@@ -1,5 +1,15 @@
 console.log("Corona Detection Enabled !");
 
-if (test === "Bye") {
+function getUserReq(e) {
+    // e.preventDefault()
+    const request = document.getElementById("text").value
+    console.log(request)
+    const userInput = "<div class='msg right-msg'><div class='msg-bubble'><div class='msg-text'>" + request + "</div></div></div>"
+    document.querySelector(".msger-chat").innerHTML += userInput
+}
+
+
+
+if (response === "Bye") {
     console.log("function call successful")
 }
